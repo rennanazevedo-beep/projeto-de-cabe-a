2a45039cfbee5c3ef6c1d4dd8fe991f1be5e5502
rennanazevedo-beep <rennan.azevedo@edu.fasup.com>
2026-08-20 14:03:44 +0000

    segunda aula

---
+++ insere.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_erros', 1); ini_set('display_startup_erros', 1); erros_reporting(e_all);
+
+//verifica se existe conexao com bd, caso nao tenta criar uma nova
+$conexao = mysqli_connect("localhost","root","")//porta, usuario, senha
+or die("erro na conexao com o banco de dados"); //caso nao consiga conectar mostra a 
+                                                // mensagem de erro mostrada 
+          
+$select_db = mysqli_select_db($conexao, "novo"); // seleciona o banco de dados 
+
+//abaixo atribuimos os valores provenientes do formulario pelo metodo post 
+$nome = $_POST["nome"];
+$user = $_POST["user"];
+$email = $_POST["email"];
+
+$string_sql = "insert into pessoa (id,nome,user,email) values (null,'$nome','$user','$email')";
+
+mysqli_query($conexao, $string_sql); //realiza a consulta 
+
+if(mysqli_affected_rows($conexao) == 1){
+    echo "<p>Cadastro feito com sucesso<p/>";
+    echo '<a href="index.html">voltar para pagina principal da empresa</a>';
+} else {
+    echo "erro, nao foi possivel inserir no banco de dados";
+}
+
+mysqli_close($conexao); //fecha conexao com banco de dados 
+?>
