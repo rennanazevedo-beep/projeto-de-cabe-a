57215496d3c2984b80d3a63abc22e20a798d9d90
rennanazevedo-beep <rennan.azevedo@edu.fasup.com>
2026-09-10 14:00:03 +0000

    quinta aula

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+   class conexao {
+                private static $instancia = null;
+    public static function getConexao(){
+        if(self::$instancia===null){
+            try{
+                self::instancia=new PDO("mysql:host=localhost;dbname=novo;","rennan","012831");
+                self::$instancia -> setAttribut(PDO)::ATTR::errmode,PDO::errmode_exception;
+            }catch (PDOexception $e){
+                die("erro na conexao ao bd:". $e -> getMessage());
+            }
+            return self::$instancia;
+        }
+    }
+   }
+?>
